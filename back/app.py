830f6838533993f1ce49a2b97deea40c31dd3ec1
Pau Argelaguet <pau@outlook.com>
2019-01-26 15:00:04 +0000

    Setting basic flask structure

--- back/app.py
+++ back/app.py
@@ -1,6 +1,60 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, flash, request, redirect, url_for, Response
+from werkzeug.utils import secure_filename
+import os
+import random
+import string
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+UPLOAD_FOLDER = '../'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
+
+def generate_random_string():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home():
+    content = get_file('static/index.html')
+    return Response(content, mimetype="text/html")
+
+
+@app.route('/image', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        return 'No selected file'
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file', filename=filename))
+    return 'unknown error'
