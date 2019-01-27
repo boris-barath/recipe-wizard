c1b8b0e8be3c2399dcd4e46d373475996e21ccf0
MatkoUSKO <pialamar@gmail.com>
2019-01-27 00:54:30 +0000

    implement returning recipe AND question. printing random sample of available recipes

--- crawl/extractor.py
+++ crawl/extractor.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import json
+import random
 
 
 class Recipe:
@@ -26,58 +27,77 @@ class Recipe:
 
 
 def return_question(reverse_mapping, recipes, available, not_available):
-    can_complete = []
+    """
+    Returns dictionary
+    {
+        available_recipe: [...],
+        next_question: <number>
+    }
 
-    for recipe in recipes:
+    """
+    to_remove = []
+    ret = {
+            "available_recipes": [],
+            "next_question": None
+          }
+
+    print("number of available recipes: ", len(recipes))
+
+    for i, recipe in enumerate(recipes):
+        for item in not_available:
+            if item in recipe.ingredients:
+                recipe.impossible()
+                to_remove.append(i)
+                continue
+
         for item in available:
             if item in recipe.ingredients:
                 recipe.ingredients.remove(item)
 
-        for item in not_available:
-            if item in recipe.ingredients:
-                recipe.impossible()
-
         if len(recipe.ingredients) == 0:
-            can_complete.append(recipe)
+            ret["available_recipes"].append(recipe)
+            to_remove.append(i)
+
+    for i in to_remove[::-1]:
+        recipes.pop(i)
 
     for ingredient in available + not_available:
         if ingredient in reverse_mapping:
             del reverse_mapping[ingredient]
 
-    if can_complete:
-        return can_complete
-    else:
-        frequencies = defaultdict(float)
+    frequencies = defaultdict(float)
 
-        for recipe in recipes:
-            for ingredient in recipe.ingredients:
-                frequencies[ingredient] += 1
+    for recipe in recipes:
+        for ingredient in recipe.ingredients:
+            frequencies[ingredient] += 1
 
-        maximum_ingredient = frequencies[max(frequencies, key=frequencies.get)]
+    maximum_ingredient = frequencies[max(frequencies, key=frequencies.get)]
 
-        for key in frequencies:
-            frequencies[key] /= maximum_ingredient
+    for key in frequencies:
+        frequencies[key] /= maximum_ingredient
 
-        difficulty = 0
-        easiest_recipe = None
+    difficulty = 0
+    easiest_recipe = None
 
-        for recipe in recipes:
-            recipe.calculate_ease(frequencies)
+    for recipe in recipes:
+        recipe.calculate_ease(frequencies)
 
-            if recipe.difficulty > difficulty:
-                difficulty = recipe.difficulty
-                easiest_recipe = recipe
+        if recipe.difficulty > difficulty and recipe.difficulty < 1:
+            difficulty = recipe.difficulty
+            easiest_recipe = recipe
 
-        ingredient_difficulty = 0
-        easiest_ingredient = None
-        for ingredient in easiest_recipe.ingredients:
-            if frequencies[ingredient] > ingredient_difficulty:
-                ingredient_difficulty = frequencies[ingredient]
-                easiest_ingredient = ingredient
+    ingredient_difficulty = 0
+    easiest_ingredient = None
+    for ingredient in easiest_recipe.ingredients:
+        if frequencies[ingredient] > ingredient_difficulty:
+            ingredient_difficulty = frequencies[ingredient]
+            easiest_ingredient = ingredient
 
-        print(difficulty, ingredient_difficulty)
+    print(difficulty, ingredient_difficulty)
 
-        return easiest_ingredient
+    ret["next_question"] = easiest_ingredient
+
+    return ret
 
 
 def get_recipes(data):
@@ -104,23 +124,32 @@ def get_data():
 def main():
     recipes, reverse_mapping = get_data()
 
+    n_of_recipes = 3
+
     available = []
     not_available = []
+    available_recipes = []
 
     while True:
         question = return_question(reverse_mapping, recipes, available, not_available)
 
-        if type(question) == str:
-            print(f'Do you have: {question}')
-        else:
-            print(f'Recipe: {question[0].id}')
+        available_recipes.extend(question["available_recipes"])
+        if available_recipes:
+            k = min(len(available_recipes), n_of_recipes)
+            suggestions = random.sample(available_recipes, k)
+
+            for recipe in suggestions:
+                available_recipes.remove(recipe)
+            suggestions = list(map(str, map(id, suggestions)))
+            print(" ".join(suggestions))
+
+        print(f'Do you have: {question["next_question"]}')
 
         response = input("Do you have this ingredient: ")
         if response == 'yes':
-            available.append(question)
+            available.append(question["next_question"])
         else:
-            not_available.append(question)
-
+            not_available.append(question["next_question"])
 
 if __name__ == "__main__":
     main()
